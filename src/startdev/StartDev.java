ca08f55afed3cceb84fc3aae9e043e5018a2f640
pupil <pupil@ivkhk.ee>
2020-10-01 15:19:50 +0300

    Добавлено меню

--- src/startdev/StartDev.java
+++ src/startdev/StartDev.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package startdev;
+import java.util.Scanner;
 
 /**
  *
@@ -15,23 +16,48 @@ public class StartDev {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//       Task1 task1 = new Task1();
-//       task1.run();
-//       
-//       Task2 task2 = new Task2();
-//       task2.run();
-//       
-//       Task3 task3 = new Task3();
-//       task3.run();
-//       
-//       Task4 task4 = new Task4();
-//       task4.run();
-//       
-//       Task5 task5 = new Task5();
-//       task5.run();
        
-//       Task6 task6 = new Task6();
-//       task6.run();
+        System.out.println("Введите номер задачи 1-7: ");
+        Scanner scanner = new Scanner(System.in);
+        int task = scanner.nextInt();
+        
+        do {
+            switch (task) {
+                 case 1: 
+                     Task1 task1 = new Task1();
+                     task1.run(); break;
+                 case 2:
+                    Task2 task2 = new Task2();
+                    task2.run(); break;
+                 case 3: 
+                     Task3 task3 = new Task3();
+                     task3.run(); break;
+//                 case 4: min = -5; max = 12; n = 30; mon = "April"; break;
+//                 case 5: min = 0; max = 17; n = 31; mon = "May"; break;
+//                 case 6: min = 5; max = 20; n = 30; mon = "June"; break;
+//                 case 7: min = 10; max = 30; n = 31; mon = "July"; break;
+//                 case 8: min = 10; max = 35; n = 31; mon = "August"; break;
+                 
+             }
+        } while (true);
+        
+        
+        
+        
+       
+       
+       
+       
+       
+       
+       Task4 task4 = new Task4();
+       task4.run();
+       
+       Task5 task5 = new Task5();
+       task5.run();
+       
+       Task6 task6 = new Task6();
+       task6.run();
        
        Task7 task7 = new Task7();
        task7.run();
